704819af0b0399a99c18b8f9730a282c1c740093
Dita Rahmane <dita.rahmane.silova@inbox.lv>
2018-11-19 23:48:54 +0200

    Add media query

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,5 +1,5 @@
 // @flow
-import * as React from "react";
+import React, { Component } from "react";
 
 import Navigation from "./Navigation";
 import Footer from "./Footer.js";
@@ -11,30 +11,47 @@ type Props = {
   children: React.Node
 };
 
-const MainPage = ({ header, transparentHeader, children, img }: Props) => {
-  const FrontPageStyle = {
-    backgroundImage: "url(" + img + ")"
-  };
+class MainPage extends Component<Props> {
+  state = {
+    show: true,
+  }
 
-  const strongImage = {
-    backgroundImage: "url(" + img + ")"
-  };
+  handleShow = () => {
+    this.setState({
+      show: !this.state.show
+    });
+    console.log('Click')
+  }
+  
+  render() {
+    const { img, transparentHeader, children, header } = this.props;
+    const FrontPageStyle = {
+      backgroundImage: "url(" + img + ")"
+    };
 
-  return (
-    <section
-      className="main-page main-page__img-container"
-      style={FrontPageStyle}>
-      <Navigation />
-      <h1>{header}</h1>
-      <div className="main-page__half-white">
-        <strong style={strongImage}>{transparentHeader}</strong>
-      </div>
-      <div className="main-page__img-container--sm" style={FrontPageStyle}>
-        {children}
-        <Footer />
-      </div>
-    </section>
-  );
+    const strongImage = {
+      backgroundImage: "url(" + img + ")"
+    };
+
+    return (
+      <section
+        className="main-page main-page__img-container"
+        style={FrontPageStyle}>
+        <div className="nav-hamburger-icon" onClick={this.handleShow}>
+          <i className="fas fa-bars"></i>
+        </div>
+        {this.state.show && (<Navigation />)}
+        <h1>{header}</h1>
+        <div className="main-page__half-white">
+          <strong style={strongImage}>{transparentHeader}</strong>
+        </div>
+        <div className="main-page__img-container--sm" style={FrontPageStyle}>
+          {children}
+          <Footer />
+        </div>
+      </section>
+    );
+  }
 };
 
 export default MainPage;
